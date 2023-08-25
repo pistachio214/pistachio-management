d583d0024a81ceb16d6f867ded2ec44bf507c507
pengsy <songyang410@outlook.com>
2023-08-25 15:51:11 +0800

    update 调整请求baseURL的字段获取方式

--- src/axios/request.ts
+++ src/axios/request.ts
@@ -6,7 +6,7 @@ import axios, {
 } from "axios";
 
 const instance: AxiosInstance = axios.create({
-    baseURL: `/${process.env.REACT_APP_BASE_URL!}`,
+    baseURL: `/${process.env.REACT_APP_BASE_URL || ""}`,
     timeout: 5000,
     headers: {
         "Content-Type": "application/json;charset=UTF-8",
